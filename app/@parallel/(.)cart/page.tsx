43e6cd968e547886c0c28aa09f13c2a1f8239ce2
Bozos2 <bozesoldo8@gmail.com>
2023-12-05 17:54:01 +0100

    parrallel routes added

---
+++ app/@parallel/(.)cart/page.tsx
@@ -0,0 +1,13 @@
+import InterceptedCartPageAsModal from "@/app/components/UI/helper/CartModal";
+
+const CartPage = () => {
+  return (
+    <section className="min-h-screen">
+      <div>
+        <InterceptedCartPageAsModal />
+      </div>
+    </section>
+  );
+};
+
+export default CartPage;
---
+++ app/@parallel/(.)profile/page.tsx
@@ -0,0 +1,13 @@
+import InterceptedPageAsModal from "@/app/components/UI/helper/ProfilePageModal";
+
+const ProfilePage = () => {
+  return (
+    <section className="min-h-screen">
+      <div>
+        <InterceptedPageAsModal />
+      </div>
+    </section>
+  );
+};
+
+export default ProfilePage;
