4c068e668a63b63b69c12fdf9b4bd73f9b9086b5
Bozos2 <bozesoldo8@gmail.com>
2023-12-09 17:34:02 +0100

    login route and illustration

---
+++ app/(router)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/app/components/Forms/LoginForm";
+
+const Login = () => {
+  return (
+    <section className="flex justify-center items-center  min-h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-stone-800 via-zinc-900 to-black">
+      <LoginForm />
+    </section>
+  );
+};
+
+export default Login;
